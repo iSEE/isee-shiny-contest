9094c6498af225240126b39819b8c54fde0adaa5
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2019-03-02 10:11:19 +0000

    include a disclaimer pointing to the devel and GitHub versions

--- app.R
+++ app.R
@@ -4,8 +4,15 @@ sce <- readRDS("allen.subset.rds")
 
 library(iSEE)
 library(shiny)
+packageVersion("iSEE")
 
 # Preload the tour, so that it opens on startup
-tour <- read.delim(system.file(package = "iSEE", "extdata", "intro_firststeps.txt"), header = TRUE, sep = ";")
-packageVersion("iSEE")
+tour <- read.delim(system.file(package = "iSEE", "extdata", "intro_firststeps.txt"), header = TRUE, sep = ";", quote = "")
+tour <- rbind(
+  data.frame(
+    element="#Disclaimer",
+    intro='<b>DISCLAIMER:</b> this demonstration app for the <a href="https://blog.rstudio.com/2019/01/07/first-shiny-contest/">iSEE shiny contest</a> uses the latest <a href="http://bioconductor.org/packages/iSEE/">release</a> version of the iSEE package on Bioconductor. A range of new exciting features are available from the the latest <a href="http://bioconductor.org/packages/devel/bioc/html/iSEE.html">development</a> version, and the very latest features (e.g., multiple selections) may be obtained directly from the <code>master</code> branch of our <a href="https://github.com/csoneson/iSEE">GitHub repository</a>.'
+  ),
+  tour
+)
 iSEE(sce, tour = tour)
