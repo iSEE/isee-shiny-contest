6d7ecd0a3eee85f547bf0e1399aaf59fb8b90dba
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2019-03-03 16:38:00 +0000

    preconfigure the app to make one of the table panels bigger

--- app.R
+++ app.R
@@ -15,4 +15,19 @@ tour <- rbind(
     intro='<b>DISCLAIMER:</b> this demonstration app for the <a href="https://blog.rstudio.com/2019/01/07/first-shiny-contest/">RStudio shiny contest</a> uses the latest <a href="http://bioconductor.org/packages/iSEE/">release</a> version of the iSEE package on Bioconductor. A range of new exciting features are available from the the latest Bioconductor <a href="http://bioconductor.org/packages/devel/bioc/html/iSEE.html">development</a> version, and the very latest features (e.g., multiple selections, voice recognition, updated UI) may be obtained directly from the <code>master</code> branch of our <a href="https://github.com/csoneson/iSEE">GitHub repository</a>.<br/><br/>We encourage you to try them out!'
   )
 )
-iSEE(sce, tour = tour)
+
+initialPanels <- DataFrame(
+    Name = c(
+      "Reduced dimension plot 1",
+      "Column data plot 1",
+      "Feature assay plot 1",
+      "Row statistics table 1",
+      "Row data plot 1",
+      "Sample assay plot 1",
+      "Column statistics table 1",
+      "Heat map 1"),
+    Width = c(4L, 4L, 4L, 4L, 4L, 4L, 8L, 4L)
+)
+
+
+iSEE(sce, tour = tour, initialPanels = initialPanels)
